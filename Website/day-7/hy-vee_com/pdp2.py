798af77dda9e7c88790b29f5b049fbe35825e6ce
shivam.soni <shivamsoni.actowiz@gmail.com>
2026-02-26 19:40:35 +0530

    Merge remote-tracking branch 'origin/main'
    done +1

---
+++ Website/day-7/hy-vee_com/pdp2.py
@@ -0,0 +1,118 @@
+import time
+import pandas as pd
+from curl_cffi import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'apollographql-client-name': 'aisles-online-web',
+    'content-type': 'application/json',
+    'origin': 'https://www.hy-vee.com',
+    'priority': 'u=1, i',
+    'referer': 'https://www.hy-vee.com/aisles-online/p/66055/certified-ground-chuck-85-lean-15-fat',
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
+    # 'x-hy-vee-correlation-id': '33151bdf-7f27-4e28-852b-4615b392a807',
+    'x-operation-name': 'getProductDetailsWithPrice',
+    # 'cookie': 'BIGipServer~Web_Administrators~Shared_VPC_pool=!HaK4Ere1GTIiaZ54QmxZpmwNFKlAzwa0onTygmym0ciHpeQ2osYAML1bq4d7SkcjgAIab0ujz0F78g==; __cf_bm=UB.mKXpJHPJfArUGBNk4uf.SinKBBasDB0ZJSYSFG5o-1772112451-1.0.1.1-Znu6zseE6oFaYMbUIkLAuPyPpcMW5m8_L0.u6C6PhSjS.VW1UR9owdnEwRVOR1A_tpDon.aG4ohlLk3F3srZ57o0eu2C2LKnp6QVmQWYz9Q; cf_clearance=Bk5iotFNPEDHR8zrSPky.fRxpwMBvJu2mkA76kmZYt4-1772112452-1.2.1.1-c49fHd.8yOzEaI8nw28ntONKcScZTbPyFAA5IjOTk0vRKzrzEGQm.OHTdWdol2LwovD91LMqsYrRchrtc7GaRemSFmafcLEbGLlrEc1SAnN61fwlbkWkHYdFT0vCZTFb6dT1UlwiyNke.ScwwB4PlFiUhNj9y5rWALSpodaD7t4fmfDuVOxTlBNO24TGRML_s4wUTO1LABv_aF3otbClFW_ZUy118lP9glY5kigFSio; store=%257B%2522address%2522%253A%25228701%2520Douglas%2520Avenue%2522%252C%2522city%2522%253A%2522Urbandale%2522%252C%2522latitude%2522%253A41.6294%252C%2522longitude%2522%253A-93.7369%252C%2522name%2522%253A%2522Urbandale%2522%252C%2522state%2522%253A%2522Iowa%2522%252C%2522storeId%2522%253A%25221759%2522%252C%2522zip%2522%253A%252250322%2522%257D; _pin_unauth=dWlkPU5UZGxOemcwTUdFdE4ySmpNQzAwTjJGaExUbGxZVEV0TXpFMVpXSTFOREZrWkdWaQ; com.silverpop.iMAWebCookie=4de9165b-086e-7ae2-8947-2528ba29b062; com.silverpop.iMA.session=1070ce42-74e0-92d1-4201-4caee6247867; __Host-hy-vee-main-hy-vee-auth.csrf-token=465f0a895c251d3e17228d5fd619310275e7a4b62863a45b32e8fe9aae05dbaa%7C8e05788504456ca67058178d2cdcc6009a98a155818964e2fe8f9054d73780c8; __Secure-hy-vee-main-hy-vee-auth.callback-url=https%3A%2F%2Fwww.hy-vee.com; _gcl_au=1.1.980378843.1772112454; _ga=GA1.1.1275940071.1772112454; hyveeSessionId=44c79014-07a6-4d0f-b2d0-3c41cbff2d99; _tt_enable_cookie=1; _ttp=01KJD24941ZCCEPN1JM754YBPY_.tt.1; _fbp=fb.1.1772112456070.538203930757108244; navigation=aisles; OptanonConsent=isGpcEnabled=0&datestamp=Thu+Feb+26+2026+18%3A57%3A50+GMT%2B0530+(India+Standard+Time)&version=202510.2.0&browserGpcFlag=0&isIABGlobal=false&hosts=&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&AwaitingReconsent=false; com.silverpop.iMA.page_visit=-807135407:47:; ttcsid=1772112454790::jOt1swzLdDLgu8RRo3Ow.1.1772112471649.0; ttcsid_C9JGOSJC77U3SHFPL600=1772112454789::0yKcEvDfuY3DxkI3hnYm.1.1772112471649.1; lastRskxRun=1772112473280; rskxRunCookie=0; rCookie=ikf54kuof98dq5bnhahqlmm3i0fk3; aHktdmVlLmNvbQ%3D%3D-_lr_tabs_-rl00u5%2Fhy-vee-prod={%22recordingID%22:%226-019c9a22-1cc8-7717-9704-331989cd1a75%22%2C%22sessionID%22:0%2C%22lastActivity%22:1772112507919%2C%22hasActivity%22:true%2C%22confirmed%22:true%2C%22recordingConditionThreshold%22:13.533584182430737%2C%22clearsIdentifiedUser%22:false}; aHktdmVlLmNvbQ%3D%3D-_lr_hb_-rl00u5%2Fhy-vee-prod={%22heartbeat%22:1772112507919}; _ga_3Q3YNGZLDP=GS2.1.s1772112453$o1$g1$t1772112509$j4$l0$h1583732714',
+}
+
+json_data1 = {
+    'operationName': 'getProductDetailsWithPrice',
+    'variables': {
+        'locationIds': [
+            '266a52f4-0e7a-4729-bc6f-25c6ebaca111',
+        ],
+        'retailItemEnabled': True,
+        'targeted': False,
+        'wicEnabled': True,
+        'foodHealthScoreEnabled': True,
+        'pickupLocationHasLocker': False,
+        'productId': 66055,
+        'storeId': 1759,
+    },
+    'query': 'query getProductDetailsWithPrice($locationIds: [ID!] = [], $retailItemEnabled: Boolean = false, $productId: Int!, $storeId: Int, $pickupLocationHasLocker: Boolean!, $targeted: Boolean = false, $wicEnabled: Boolean = false, $foodHealthScoreEnabled: Boolean = false) {\n  product(productId: $productId) {\n    productId\n    size\n    productLockers @include(if: $pickupLocationHasLocker) {\n      productLockerId\n      pickupLocationId\n      isLockerEligible\n      __typename\n    }\n    couponProductV4(targeted: $targeted) {\n      couponsV4 {\n        couponId\n        offerState\n        valueText\n        __typename\n      }\n      __typename\n    }\n    item {\n      ecommerceDefaultPrice\n      nutrition @include(if: $foodHealthScoreEnabled) {\n        score\n        activeBoosters {\n          code\n          title\n          value\n          description\n          __typename\n        }\n        activeDetractors {\n          code\n          title\n          value\n          description\n          __typename\n        }\n        __typename\n      }\n      ...IItemFragment\n      retailItems(locationIds: $locationIds) @include(if: $retailItemEnabled) {\n        ...IRetailItemFragment\n        __typename\n      }\n      madeToOrder {\n        mtoItems {\n          mtoModifiers {\n            mtoModifierId\n            options {\n              code\n              amount\n              __typename\n            }\n            __typename\n          }\n          fulfillmentBeginDate\n          fulfillmentEndDate\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  storeProducts(where: {productId: $productId, storeId: $storeId, isActive: true}) {\n    storeProducts {\n      storeProductId\n      productId\n      storeId\n      onSale\n      onFuelSaver\n      isWeighted\n      isAlcohol\n      fuelSaver\n      price\n      priceMultiple\n      basePrice\n      basePriceMultiple\n      isTagPriceLower\n      department {\n        departmentId\n        __typename\n      }\n      storeProductDescriptions {\n        type\n        description\n        __typename\n      }\n      subcategoryId\n      departmentGroup {\n        departmentGroupId\n        linkPath\n        name\n        __typename\n      }\n      department {\n        departmentId\n        linkPath\n        name\n        __typename\n      }\n      category {\n        categoryId\n        departmentId\n        linkPath\n        name\n        subcategories {\n          subcategoryId\n          linkPath\n          name\n          __typename\n        }\n        __typename\n      }\n      variations {\n        name\n        variationsAttributes {\n          name\n          variationsProducts {\n            productId\n            product {\n              productId\n              name\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment IWicItemFragment on WicItem {\n  isCvb\n  isBroadbandAllowed\n  wicExchangeRate\n  wicItemId\n  wicSubcategory {\n    categoryCode\n    categoryDescription\n    subcategoryCode\n    subcategoryDescription\n    unitOfMeasure\n    isBroadbandSubcategory\n    __typename\n  }\n  upcHyVee\n  __typename\n}\n\nfragment IMadeToOrderItemFragment on MtoItem {\n  mtoItemId\n  prepTime\n  fulfillmentBeginDate\n  fulfillmentEndDate\n  messages {\n    name\n    __typename\n  }\n  mtoModifiers {\n    options {\n      amount\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment IItemFragment on Item {\n  itemId\n  description\n  ecommerceStatus\n  source\n  images {\n    imageId\n    url\n    isPrimaryImage\n    __typename\n  }\n  source\n  unitAverageWeight\n  WicItems(locationIds: $locationIds) @include(if: $wicEnabled) {\n    ...IWicItemFragment\n    __typename\n  }\n  madeToOrder {\n    mtoItems {\n      ...IMadeToOrderItemFragment\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment IRetailItemFragment on RetailItem {\n  retailItemId\n  basePrice\n  basePriceQuantity\n  soldByUnitOfMeasure {\n    code\n    name\n    __typename\n  }\n  tagPrice\n  tagPriceQuantity\n  ecommerceTagPrice\n  ecommerceTagPriceQuantity\n  memberTagPrice\n  memberTagPriceQuantity\n  sellingRules\n  __typename\n}\n',
+}
+
+token = "your"
+proxyModeUrl = "your"
+proxies = {
+    "http": proxyModeUrl,
+    "https": proxyModeUrl,
+}
+
+results_data = []
+url = 'https://www.hy-vee.com/aisles-online/api/graphql/two-legged/getProductDetailsWithPrice'
+
+def make_request(i, max_retries=3, backoff=2):
+    for attempt in range(1, max_retries + 1):
+        try:
+            r = requests.post(
+           url,
+           json=json_data1,
+           headers=headers,
+           # cookies=cookies,
+           impersonate="chrome120",
+           timeout=200,
+                proxies=proxies,
+                verify=False
+       )
+
+            credit = r.headers.get('scrape.do-request-cost', 'NA')
+
+            if 'Certified Ground Chuck 85% Lean 15% Fat' in r.text and '5.99' in r.text:
+                return {
+                    "request_no": i,
+                    "attempt": attempt,
+                    "status": "GOOD",
+                    "http_status": r.status_code,
+                    "credit": credit
+                }
+
+            if attempt < max_retries:
+                time.sleep(backoff ** attempt)
+                continue
+
+            return {
+                "request_no": i,
+                "attempt": attempt,
+                "status": "BAD",
+                "http_status": r.status_code,
+                "credit": credit
+            }
+
+        except Exception as e:
+            if attempt == max_retries:
+                return {
+                    "request_no": i,
+                    "attempt": attempt,
+                    "status": "ERROR",
+                    "http_status": "NA",
+                    "credit": "NA",
+                    "error": str(e)
+                }
+
+            time.sleep(backoff ** attempt)
+    return None
+
+if __name__ == "__main__":
+    num_requests = 1000
+    max_workers = 100
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(make_request, i) for i in range(1, num_requests + 1)]
+        for future in as_completed(futures):
+            result = future.result()
+            results_data.append(result)
+            print(result)
+
+    df = pd.DataFrame(results_data)
+    df.to_excel("date_availability_report.xlsx", index=False)
+
+    print("Excel file saved")
---
+++ Website/day-7/schnucks_com/pdp2.py
@@ -0,0 +1,100 @@
+import time
+import pandas as pd
+from curl_cffi import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    # 'if-none-match': '"172yqd3yzdn1to4"',
+    'priority': 'u=0, i',
+    'referer': 'https://schnucks.com/shop/bakery/breakfast-pastries',
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
+    # 'cookie': 'TS01b763b5=0182a5a6cf8580c9ce248373fae9b93903246b0623905cb2bace370917a8e61b5783714167468eef1d90c5ae8f1595329a6d2965f0; _fbp=fb.1.1772113910298.734946929833270260; _gid=GA1.2.1304019095.1772113911; _pin_unauth=dWlkPU5qVXpZall5WldJdE1qVXlOUzAwTmpKaUxXSmlaV0V0TVRFek5tVTFZMkptWkRobA; __privaci_cookie_consent_uuid=b499cf06-296a-4762-a674-186f88c6f1ac:4; __privaci_cookie_consent_generated=b499cf06-296a-4762-a674-186f88c6f1ac:4; _clck=1s8etnc%5E2%5Eg3w%5E0%5E2248; _tt_enable_cookie=1; _ttp=01KJD3H0ET8739F0T2E7W44S5P_.tt.1; _gcl_au=1.1.925421708.1772113921; __privaci_cookie_consents={"consents":{"1":1,"2":1,"4":1,"5":1},"location":"GJ#IN","lang":"en","gpcInBrowserOnConsent":false,"gpcStatusInPortalOnConsent":false,"status":"record-consent-success","implicit_consent":false}; __privaci_latest_published_version=5; ajs_anonymous_id=466e4b25-d10a-4c1c-9b89-b11d70fd7eb8; _conv_v=vi%3A1*sc%3A1*cs%3A1772113911*fs%3A1772113911*pv%3A7*exp%3A%7B%7D*seg%3A%7B%7D; _conv_s=sh%3A1772113910784-0.6276341739789632*si%3A1*pv%3A7; _gat_UA-3017216-1=1; _gat_%5Bobject%20Object%5D=1; _gat_gtag_UA_3017216_1=1; _ga=GA1.1.1169088979.1772113911; _ga_VS0TEWYZ5P=GS2.1.s1772113911$o1$g1$t1772113993$j60$l0$h0; _clsk=kkpz7m%5E1772113994318%5E6%5E1%5El.clarity.ms%2Fcollect; ttcsid=1772113920489::qeTY7sgt3y6xq5uvLFfb.1.1772113994326.0; ttcsid_CD7AQCJC77U5U64QBH8G=1772113920486::fFE7EOd3xPbld7M7lQvC.1.1772113994326.1; datadome=fi2T0txUSopa0Jrb9WWq815Cj8HfDP2JlfuhAeuzf5NIV3eceBroakLseuTmb3yV6yKGWTxPRK7rtkrz_eQ3CdT6siD9Y7tBSuSQQ4V8JVpAAIHHgqI688auEPRqGu0y',
+}
+
+token = "your"
+proxyModeUrl = "your"
+proxies = {
+    "http": proxyModeUrl,
+    "https": proxyModeUrl,
+}
+
+results_data = []
+url ='https://schnucks.com/products/1121586/schnucks-6-count-assorted-cake-donut-14-oz'
+
+def make_request(i, max_retries=3, backoff=2):
+    for attempt in range(1, max_retries + 1):
+        try:
+            r = requests.get(
+                url,
+                # params=params,
+                headers=headers,
+                # cookies=cookies,
+                impersonate="chrome120",
+                timeout=200,
+                proxies=proxies,
+                verify=False
+       )
+
+            credit = r.headers.get('scrape.do-request-cost', 'NA')
+
+            if 'Schnucks - 6 Count Assorted Cake Donut (14 Oz)' in r.text and '5.99' in r.text:
+                return {
+                    "request_no": i,
+                    "attempt": attempt,
+                    "status": "GOOD",
+                    "http_status": r.status_code,
+                    "credit": credit
+                }
+
+            if attempt < max_retries:
+                time.sleep(backoff ** attempt)
+                continue
+
+            return {
+                "request_no": i,
+                "attempt": attempt,
+                "status": "BAD",
+                "http_status": r.status_code,
+                "credit": credit
+            }
+
+        except Exception as e:
+            if attempt == max_retries:
+                return {
+                    "request_no": i,
+                    "attempt": attempt,
+                    "status": "ERROR",
+                    "http_status": "NA",
+                    "credit": "NA",
+                    "error": str(e)
+                }
+
+            time.sleep(backoff ** attempt)
+    return None
+
+if __name__ == "__main__":
+    num_requests = 1000
+    max_workers = 100
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(make_request, i) for i in range(1, num_requests + 1)]
+        for future in as_completed(futures):
+            result = future.result()
+            results_data.append(result)
+            print(result)
+
+    df = pd.DataFrame(results_data)
+    df.to_excel("date_availability_report.xlsx", index=False)
+
+    print("Excel file saved")
